5a87f86671d224e1f5ebab72177ab90fd156f400
Louis-Charles Patry <lcpatry@yahoo.ca>
2026-02-16 14:13:44 -0500

    chore(app): ajouter le bon chemin de fichier pour le html et css

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
 from backend.operators import add, subtract, multiply, divide
+import os
 
-app = Flask(__name__)
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'static'))
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 OPS = {
     '+': add,
